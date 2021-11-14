6eaf21909fd6db911f0441594969566e0d55be7f
ADRIAN RADU RADULESCU <adrian.radulescu@s.unibuc.ro>
2021-11-14 09:34:44 +0200

    Add project files.

---
+++ ProiectDAW.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using ProiectDAW.API.Models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+namespace ProiectDAW.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public AuthenticationController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        #region Login
+        [AllowAnonymous]
+        [Route("[action]")]
+        [HttpPost]
+        public IActionResult Login([FromBody] User login)
+        {
+            IActionResult response = Unauthorized();
+            var user = AuthenticateUser(login);
+
+            if (user != null)
+            {
+                var tokenString = GenerateJSONWebToken(user);
+                response = Ok(new { token = $"Bearer {tokenString}" });
+            }
+
+            return response;
+        }
+
+        private string GenerateJSONWebToken(User userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            //Data that the token contains
+            var claims = new[] 
+            {
+                new Claim(type: "Username", userInfo.Username),
+                new Claim(type: "Email", userInfo.EmailAddress),
+                new Claim(type: "DateOfJoing", userInfo.DateOfJoing.ToString("yyyy-MM-dd")),
+                new Claim(ClaimTypes.Role, userInfo.Role),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+                                             _config["Jwt:Issuer"],
+                                             claims,
+                                             expires: DateTime.Now.AddMinutes(120),
+                                             signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private User AuthenticateUser(User login)
+        {
+            User user = null;
+
+            //TODO: Implement DB Logic
+            //Validate the User Credentials    
+            //Demo Purpose, I have Passed HardCoded User Information    
+            if (login.Username == "admin")
+            {
+                user = new User { Username = "Gigel Frone", EmailAddress = "sloboz@moloz.com", Role = Role.Admin };
+            }
+            else user = new User { Username = "Gigel Frone", EmailAddress = "sloboz@moloz.com", Role = Role.User };
+            
+            return user;
+        }
+        #endregion
+
+
+        //TODO: Replace With Real Actions
+        //Multiple roles [Authorize(Roles = "HRManager,Finance")]
+        [HttpGet]
+        [Route("[action]")]
+        [Authorize(Roles = Role.Admin)]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var currentUser = HttpContext.User;
+            int spendingTimeWithCompany = 0;
+
+            if (currentUser.HasClaim(c => c.Type == "DateOfJoing"))
+            {
+                DateTime date = DateTime.Parse(currentUser.Claims.FirstOrDefault(c => c.Type == "DateOfJoing").Value);
+                spendingTimeWithCompany = DateTime.Today.Year - date.Year;
+            }
+
+            if (spendingTimeWithCompany > 5)
+            {
+                return new string[] { "High Time1", "High Time2", "High Time3", "High Time4", "High Time5" };
+            }
+            else
+            {
+                return new string[] { "value1", "value2", "value3", "value4", "value5" };
+            }
+        }
+    }
+}
---
+++ ProiectDAW.API/Startup.cs
@@ -0,0 +1,108 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using ProiectDAW.API.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProiectDAW.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DatabaseContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("Connection")));
+
+            services.AddAuthentication(x=>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    ValidAudience = Configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                };
+            });
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProiectDAW.API", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Format:   Bearer {Token}",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement 
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] { }
+                    }
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProiectDAW.API v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
