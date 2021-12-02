682918b1c40f17342168a21ed467953c75d374d7
Radulescu Adrian <adi_rules@yahoo.com>
2021-12-02 05:48:58 +0200

    initial changes

--- ProiectDAW/src/app/app.module.ts
+++ ProiectDAW/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/account/login/login.component';
+import { RegisterComponent } from './components/account/register/register.component';
+import { ProfileComponent } from './components/account/profile/profile.component';
+import { GetProductsComponent } from './components/product/get-products/get-products.component';
+import { GetProductComponent } from './components/product/get-product/get-product.component';
+import { UpdateProductComponent } from './components/product/update-product/update-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    GetProductsComponent,
+    GetProductComponent,
+    UpdateProductComponent
   ],
   imports: [
     BrowserModule,
